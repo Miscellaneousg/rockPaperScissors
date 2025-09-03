b1b0baa5546b2b9720d80450a1eac4a482192e86
Sahil Yadav <sahil15785@gmail.com>
2025-09-03 14:27:36 +0000

    Write skeleton functions - scaffolding
    - playGame()
    - playRound()
    - getHumanChoice()
    - getComputerChoice()

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-console.log("hello, world");
+//main function
+function playGame(){
+}
+
+
+//Function to check round winner 
+function playRound(){ 
+}
+
+//Function to return human choice (rock, paper or scissors).
+function getHumanChoice(){ 
+}
+
+//Function to return computer choice (rock, paper or scissors).
+function getComputerChoice(){ 
+}
