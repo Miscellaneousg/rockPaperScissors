0bdf27d2848b36af285576439205437a4eedf919
Sahil Yadav <sahil15785@gmail.com>
2025-09-03 14:45:26 +0000

    Write logic for playRound()
    - main game logic is implemented for rock, paper,
      scissors game.

--- scripts.js
+++ scripts.js
@@ -4,7 +4,18 @@ function playGame(){
 
 
 //Function to check round winner 
-function playRound(){ 
+function playRound(humanChoice, computerChoice){
+
+    if(humanChoice==="rock" && computerChoice==="scissors"){
+        return 0;
+    }else if(humanChoice==="paper" && computerChoice==="rock"){
+        return 0;
+    }else if(humanChoice==="scissors" && computerChoice==="paper"){
+        return 0;
+    }else if(humanChoice===computerChoice){
+        return 2;
+    }
+    return 1; 
 }
 
 //Function to return human choice (rock, paper or scissors).
