ac9a95218f5e4971584b878857a823baf7089361
Sahil Yadav <sahil15785@gmail.com>
2025-09-03 14:38:00 +0000

    Write logic for getComputerChoice()
    - Use Math.random to get random outcomes
    - Generates values from 0 to 2
    - Assign each number to a outcome, choice from
      computer

--- scripts.js
+++ scripts.js
@@ -12,5 +12,15 @@ function getHumanChoice(){
 }
 
 //Function to return computer choice (rock, paper or scissors).
-function getComputerChoice(){ 
+function getComputerChoice(){
+
+    let random = Math.floor(Math.random()*3);
+
+    if(random===0){
+        return "rock";
+    }else if(random===1){
+        return "paper";
+    }else{
+        return "scissors";
+    } 
 }
