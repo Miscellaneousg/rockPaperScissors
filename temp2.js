bcf27651b8de2d0139b67209fe06aa452d6bb3d8
Sahil Yadav <sahil15785@gmail.com>
2025-09-18 15:45:36 +0000

    Section game screen, transitions,  pseudo-classes
    - Added div's for computer, player and score.
    
    - Added range type input for initial total round
      selection.
    
    - Added button options for rock, paper and scissor
    
    - Used hover pseudo class and removed mouseenter
      and mouseleave events, lesser javascript.
    
    - More transitions to play and exit buttons.
    
    - Started building logic to handle DOM and assets
      for main game (games screens, computer-player-
      score div's).
    
    many changes ;D

--- temp2.js
+++ temp2.js
@@ -15,7 +15,7 @@ const updateCircleGeometry = (button)=>{
     // Position the circle
     circle.style.left = relativeX + 'px';
     circle.style.top = relativeY + 'px';
-}
+};
 
 window.addEventListener('load', ()=>updateCircleGeometry(buttons[0]));
 window.addEventListener('resize',()=>updateCircleGeometry(buttons[0]));
@@ -25,7 +25,6 @@ buttons[0].addEventListener("click",()=>{
     circle.classList.toggle("ripple");
     circle.classList.toggle("expanding");
     setTimeout(() => {
-        // circle.classList.toggle("expanding");
         container.style.display="none";
         container.nextElementSibling.style.display="flex";
     },700);
@@ -41,22 +40,98 @@ buttons[1].addEventListener("click",()=>{
     },900);
 });
 
-buttons.forEach((button)=>{
-    button.addEventListener("mouseenter",()=>{
-    button.classList.toggle("glow");
-    button.style.color="red";
-    });
-    button.addEventListener("mouseleave",()=>{
-    button.classList.toggle("glow");
-    button.style.color="rgb(118, 118, 196)";
-    });
-});
-
     // container.style.display="none";
     // container.nextElementSibling.style.display="flex";
 
 
 //-----------------------------------------------------------------------//
+
+const round = document.querySelector("input#round");
+
+round.addEventListener("input", ()=>document.querySelector("label.round").textContent=`Total Rounds : ${round.value}`);
+
+//Game variables
+let humanChoice;
+let computerChoice;
+let humanScore;
+let computerScore;
+let curRound;
+let rounds;
+document.querySelector("#buttons").style.display="none";
+//Initialize 
+function start(round){
+    document.querySelector("#buttons").style.display="flex";
+    humanChoice=null;
+    computerChoice=null;
+    humanScore=0;
+    computerScore=0;
+    curRound=0;
+    rounds=round;
+}
+
+
+    // while(curRound<rounds){
+    //     //Get human choice
+    //     humanChoice = getHumanChoice(curRound,rounds);      
+    //     if(humanChoice===undefined)return;
+    //     //Get computer choice
+    //     computerChoice = getComputerChoice().toLowerCase();
+    //     //Call function playRound to check who win the round
+    //     let win = playRound(humanChoice.toLowerCase(), computerChoice);
+    //     //Update scores based on win, O is win for human and 1 for computer
+    //     if(win===0){
+    //         humanScore++;
+    //     }else if(win===1){
+    //         computerScore++;
+    //     }
+    //     //Dispaly live score
+    //     liveScoreBoard(curRound,rounds,humanChoice,computerChoice,humanScore,computerScore);
+    //     //Go to next round
+    //     curRound++;
+    // }
+    // //End Display result
+    // Result(humanScore,computerScore);    
+
+//Display current round results
+function liveScoreBoard(curRound,rounds,humanChoice,computerChoice,humanScore,computerScore){
+
+        alert(`Round ${curRound+1}/${rounds}
+You: ${humanChoice}
+Computer: ${computerChoice}
+Score → Human: ${humanScore} | Computer: ${computerScore}`);
+}
+
+//Display the winner
+function Result(humanScore,computerScore){
+
+    if(humanScore<computerScore){
+
+    }else if(humanScore==computerScore){
+
+    }
+    else{
+        
+    }    
+}
+
+//Function to check round winner 
+function playRound(humanChoice, computerChoice){
+
+    if(humanChoice==="rock" && computerChoice==="scissors"){
+        return 0;
+    }else if(humanChoice==="paper" && computerChoice==="rock"){
+        return 0;
+    }else if(humanChoice==="scissors" && computerChoice==="paper"){
+        return 0;
+    }else if(humanChoice===computerChoice){
+        return 2;
+    }
+    return 1; 
+}
+
+//Function to return human choice (rock, paper or scissors).
+function getHumanChoice(curRound, rounds){ 
+}
 
 //Function to return computer choice (rock, paper or scissors).
 function getComputerChoice(){
